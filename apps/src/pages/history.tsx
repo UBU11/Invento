db8db1623b8caae0a3f9366a460e102eb1872642
UBU11 <uvaisubu11@gmail.com>
2026-01-01 17:48:49 +0530

    feat: horizontal scrolling effect for History section

--- apps/src/pages/history.tsx
+++ apps/src/pages/history.tsx
@@ -1,14 +1,58 @@
 
+"use client";
 
-import HistoryText from "../components/Text/historyText"
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-export default function History (){
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
 
-  return(
+export default function History() {
+  const containerRef = useRef<HTMLElement>(null);
+  const spacerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!spacerRef.current || !containerRef.current) return;
+
+    const ctx = gsap.context(() => { 
+ 
+      gsap.fromTo(
+        containerRef.current,
+        {
+          xPercent: 50, 
+        },
+        {
+          xPercent: -50,
+          ease: "none",
+          scrollTrigger: {
+            trigger: spacerRef.current,
+            start: "top center",
+            end: "bottom bottom",
+            scrub: 1,
+            invalidateOnRefresh: true,
+            markers: { startColor: "blue", endColor: "orange", fontSize: "18px", indent: 200 },
+          },
+        }
+      );
+    }, spacerRef);
+
+    return () => ctx.revert();
+  }, []);
+
+
+  return (
     <>
-    <section className="bg-white overflow-hidden h-screen">
-    <HistoryText text="OUR HISTORY"/>
-    </section>
+      <div ref={spacerRef} className="h-[200vh] w-full relative -z-10 bg-transparent" />
+      <section
+        ref={containerRef}
+        className=" fixed top-0 left-0 h-screen w-[200vw] z-50 bg-white flex"
+      >
+        <div className=" w-screen h-screen shrink-0 flex justify-start items-end relative p-20">
+           <h1 className=" text-8xl font-bold">OUR HISTORY</h1>
+        </div>
+      </section>
     </>
-  )
+  );
 }
